35f5561e32bbe083bacd658c0cd33482d4e803aa
rPhilip <rphilip21@gmail.com>
2017-07-22 12:59:35 -0700

    AoC 9 pt 2 working on solution

--- src/main/java/AoC09.java
+++ src/main/java/AoC09.java
@@ -7,16 +7,84 @@ import java.io.IOException;
  */
 public class AoC09 {
 
-    static int AoC09pt2() throws IOException {
+    static long AoC09pt2() throws IOException {
         FileReader fr = new FileReader("AoC09.txt");
         BufferedReader br = new BufferedReader(fr);
         String currentLine;
-        int decompressedLength = 0;
-        String decompressedString = "";
+        long decompressedLength = 0;
         while ((currentLine = br.readLine()) != null) {
 
+            //iterate through the string
+            for(int i = 0; i < currentLine.length(); i++) {
+                long prevDecompressed = decompressedLength;
+                if (currentLine.charAt(i) == '(') {
+                    //we need to do multiplication
+                    //get the substring from i (we don't need to look back at already-processed letters)
+                    String substring = currentLine.substring(i);
+                    //get the decompressed length. We set the second param to true because we are looking at the outer
+                    //part of the equation
+                    decompressedLength += calculateDecompressedSize(substring, true);
+                    //get the length of the current section analyzed
+                    String[] parts = substring.split("\\(|\\)");
+                    String[] dimensions = parts[1].split("x");
+                    int increment = Integer.parseInt(dimensions[0]) + parts[1].length() + 1;
+                    //increment i so that we don't re-analyze what we've already looked at.
+                    i += increment;
+                } else {
+                    //it's just a letter we count
+                    decompressedLength++;
+                }
+                if (prevDecompressed > decompressedLength) {
+                    System.out.println("OVERFLOW!!!");
+                }
+            }
         }
         return decompressedLength;
+    }
+
+    static long calculateDecompressedSize(String str, boolean isOuter) {
+        //get the parameters
+        String[] parts = str.split("\\(|\\)");
+        String[] dimensions = parts[1].split("x");
+        int numChars = Integer.parseInt(dimensions[0]);
+        int numRepetitions = Integer.parseInt(dimensions[1]);
+        int charsToTrim = parts[1].length() + 2;
+        String trimmedStr = str.substring(charsToTrim); //takes of first set of parens for this substring
+
+        boolean isAdditive = false;
+
+        String checkStr = trimmedStr.substring(0, numChars); //this is the string within the numChars
+
+        if (trimmedStr.charAt(0) != '(') {
+            isAdditive = true;
+            trimmedStr = trimmedStr.substring(numChars);
+        } else {
+            trimmedStr = trimmedStr.substring(0, numChars);
+        }
+
+        if (isOuter) {
+            if (doesStrContainParens(checkStr)) {
+                if (isAdditive) {
+                    return numRepetitions * numChars + calculateDecompressedSize(trimmedStr, false);
+                } else {
+                    return numRepetitions * calculateDecompressedSize(trimmedStr, false);
+                }
+            }
+        } else {
+            if (doesStrContainParens(trimmedStr)) {
+                if (isAdditive) {
+                    return numRepetitions * numChars + calculateDecompressedSize(trimmedStr, false);
+                } else {
+                    return numRepetitions * calculateDecompressedSize(trimmedStr, false);
+                }
+            }
+        }
+
+        return numChars * numRepetitions;
+    }
+
+    static boolean doesStrContainParens(String str) {
+        return str.contains("(");
     }
 
     static public int AoC09pt1() throws IOException {
@@ -80,6 +148,7 @@ public class AoC09 {
 
     static public void main(String[] args) throws IOException {
         System.out.println("The solution to AoC09.1 is: " + AoC09pt1());
+        System.out.println("The solution to AoC09.2 is: " + AoC09pt2());
     }
 }
 
